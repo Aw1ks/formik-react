8aa0b352f015c7c27d91c24cd52ffa3cfa045337
Aw1ks <aw1ksss331@gmail.com>
2025-10-26 11:02:42 +0300

    Drew the "Form" componenet

---
+++ src/components/form/Form.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Form() {
+    return (
+        <form className="Form">
+            <h1 className="Form-Welcome">Welcome</h1>
+        </form>
+    )
+}
+
+export default Form;
